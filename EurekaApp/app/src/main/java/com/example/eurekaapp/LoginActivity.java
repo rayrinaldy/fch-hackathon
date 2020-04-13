b877bb64d80966d7d73d4b4e472c7b7a6b75b774
NitinMa2 <nitinmathew.official@gmail.com>
2020-04-13 19:08:33 +1000

    Login & Register UI done

--- EurekaApp/app/src/main/java/com/example/eurekaapp/LoginActivity.java
+++ EurekaApp/app/src/main/java/com/example/eurekaapp/LoginActivity.java
@@ -2,13 +2,35 @@ package com.example.eurekaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText etEmail, etPassword;
+    TextView tvRegister;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmail = (EditText) findViewById(R.id.emailField);
+        etPassword = (EditText) findViewById(R.id.passwordField);
+        btnLogin = (Button) findViewById(R.id.loginButton);
+        tvRegister = (TextView) findViewById(R.id.registerHereButton);
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- EurekaApp/app/src/main/java/com/example/eurekaapp/RegisterActivity.java
+++ EurekaApp/app/src/main/java/com/example/eurekaapp/RegisterActivity.java
@@ -2,13 +2,42 @@ package com.example.eurekaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    TextView tvLoginHere;
+    EditText etFirstName, etLastName, etRegisterEmail, etNumber, etRegisterPassword;
+    CheckBox chkObservation, chkPositive;
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etFirstName = (EditText) findViewById(R.id.firstNameField);
+        etLastName = (EditText) findViewById(R.id.lastNameField);
+        etRegisterEmail = (EditText) findViewById(R.id.registerEmailField);
+        etNumber = (EditText) findViewById(R.id.numberField);
+        etRegisterPassword = (EditText) findViewById(R.id.registerPasswordField);
+        chkObservation = (CheckBox) findViewById(R.id.observationField);
+        chkPositive = (CheckBox) findViewById(R.id.positiveCheck);
+        btnRegister = (Button) findViewById(R.id.registerButton);
+        tvLoginHere = (TextView) findViewById(R.id.loginHereButton);
+
+        tvLoginHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
