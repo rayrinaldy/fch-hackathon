d5f85cfc8fc73fb5e61a5d42f3f354dda8683797
Ray <rayrinaldy@gmail.com>
2020-04-14 02:17:24 +0000

    wip

--- covid19-backend/ecosystem.config.js
+++ covid19-backend/ecosystem.config.js
@@ -7,24 +7,6 @@ module.exports = {
     args: 'develop',
     instances: 1,
     autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
+    watch: true,
   }],
-
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
 };
