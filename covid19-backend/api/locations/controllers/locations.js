e49b63a6230177daa00ae74b042002764305649e
Ray <rayrinaldy@gmail.com>
2020-04-14 15:18:39 +0000

    added proximity

--- covid19-backend/api/locations/controllers/locations.js
+++ covid19-backend/api/locations/controllers/locations.js
@@ -5,4 +5,44 @@
  * to customize this controller
  */
 
-module.exports = {};
+const geodist = require('geodist');
+
+module.exports = {
+    updateUserLocation: async ctx => {
+        let userId = ctx.params.id;
+        
+        let infectedUser = await strapi.query('user', 'users-permissions').find({id: userId})
+        let allUsers = await strapi.plugins['users-permissions'].services.user.fetchAll();
+
+        let infectedUserCoordinate = infectedUser[0].locations[0].coordinate;
+        let infectionProximity = [];
+
+        await allUsers.forEach(el => {
+            if(el.locations.length > 0) {
+                let rawData = geodist(infectedUserCoordinate, el.locations[0].coordinate, {exact: true, unit: 'km'});
+                let calculatedData = Math.ceil(rawData * 1000); //convert to KM
+
+                if (calculatedData < 1000 && calculatedData != 0) {
+                    strapi
+                        .query('user', 'users-permissions')
+                        .update({_id: el._id}, {
+                            hasBeenInCloseProximity: true
+                        })
+                    
+                    infectionProximity.push({
+                        _id: el._id,
+                        proximity: calculatedData,
+                        fullName: el.firstName + ' ' + el.lastName,
+                        phoneNo: el.phoneNo,
+                        underObservation: el.underObservation,
+                        isCovidPositive: el.isCovidPositive,
+                        lastKnownLocation: el.locations[el.locations.length - 1],
+                        hasBeenInCloseProximity: el.hasBeenInCloseProximity
+                    })
+                }
+            }
+        });
+
+        return infectionProximity
+    }
+};
