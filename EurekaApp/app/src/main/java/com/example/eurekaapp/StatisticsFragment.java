909ad851255c7fd06d3a08f49447a105a2f0fbb7
Yash <yasheenmpeiris@gmail.com>
2020-04-14 19:01:39 +1000

    Graph update

--- EurekaApp/app/src/main/java/com/example/eurekaapp/StatisticsFragment.java
+++ EurekaApp/app/src/main/java/com/example/eurekaapp/StatisticsFragment.java
@@ -10,10 +10,37 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
-public class StatisticsFragment extends Fragment {
-    @Nullable
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
+public class StatisticsFragment extends Fragment {
+    LineGraphSeries<DataPoint> series;
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+    @Nullable
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_statistics,container,false);
+        double y,x;
+        x = -5.0;
+        View rootView =  inflater.inflate(R.layout.fragment_statistics,container,false);
+        GraphView graph = (GraphView) rootView.findViewById(R.id.graph);
+
+        graph.setTitle("Total Confirmed Cases");
+        GridLabelRenderer gridLabel = graph.getGridLabelRenderer();
+        gridLabel.setHorizontalAxisTitle("Date");
+        gridLabel.setVerticalAxisTitle("Number of Cases");
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+        return rootView;
     }
 }
